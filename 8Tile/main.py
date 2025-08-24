6e6563476fcdd7bab8b06f1a5831dce69f291a3c
eliasfallahWSU <22111881@student.westernsydney.edu.au>
2025-08-24 02:09:35 +0000

    Complete 8 tile a* algorithm

--- 8Tile/main.py
+++ 8Tile/main.py
@@ -1,29 +1,41 @@
 from collections import deque
+import heapq
 import numpy as np
 
 goalState = ([1, 2, 3],
              [8, 0, 4],
              [7, 6, 5])
 
-# initialState = ([2, 8, 3],
-#                 [1, 6, 4],
-#                 [7, 0, 5])
+initialState5 = ([2, 8, 3],
+                 [1, 6, 4],
+                 [7, 0, 5])
 
-# 1 move initial state
-initialState = ([1, 2, 3],
-                [0, 8, 4],
-                [7, 6, 5])
+initialState18 = ([2, 1, 6],
+                  [4, 0, 8],
+                  [7, 5, 3])
+
+initialState21 = ([5, 7, 2],
+                  [0, 8, 6],
+                  [4, 1, 3])
+
 
 class EightPuzzle:
-    def __init__(self, board):
+    def __init__(self, board, parent=None, gVal=0):
         self.board = board
+        self.parent = parent
         self.bSize = 3
         self.goalState = (
             [1, 2, 3],
             [8, 0, 4],
             [7, 6, 5]
         )
+        self.gVal = gVal
+        self.hVal = self.manhattanDistance()
+        self.fVal = self.gVal + self.hVal
 
+
+    def __gt__(self, other):
+        return self.fVal > other.fVal
 
     def isGoalState(self):
         return np.array_equal(self.board, self.goalState)
@@ -34,10 +46,11 @@ class EightPuzzle:
 
         return possibleMoves
 
-    def validMove(self, newRow, newCol):
-        return 0 <= newRow < self.bSize and 0 <= newCol < self.bSize
+    def validMove(self, row, col):
+        return 0 <= row < self.bSize and 0 <= col < self.bSize
 
     def makeMove(self, newBoard):
+        self.gVal += 1
         self.board = newBoard
 
     def printBoard(self):
@@ -45,11 +58,15 @@ class EightPuzzle:
             print(" ----------------")
             for j in range(self.bSize):
                 print(" | ", end="")
-                print(self.board[i][j], end=" ")
+                if self.board[i][j] == 0:
+                    print(" ", end=" ")
+                else:
+                    print(self.board[i][j], end=" ")
                 if j == self.bSize - 1:
                     print(" | ")
         print(" ----------------")
-        
+        print()
+
     def getBlankTilePosition(self):
         for i in range(self.bSize):
             for j in range(self.bSize):
@@ -59,7 +76,8 @@ class EightPuzzle:
     def getPossibleMoves(self):
         possibleMoves = []
         blankPos = self.getBlankTilePosition()
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
+                       # Up,    Down,    Left,   Right
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  
 
         for d in directions:
             newRow = blankPos[0] + d[0]
@@ -68,19 +86,56 @@ class EightPuzzle:
                 newBoard = np.array(self.board, copy=True)
                 newBoard[blankPos[0]][blankPos[1]] = newBoard[newRow][newCol]
                 newBoard[newRow][newCol] = 0
-                possibleMoves.append(EightPuzzle(newBoard))
+                possibleMoves.append(EightPuzzle(board=newBoard, parent=self, gVal=self.gVal + 1))
 
         return possibleMoves
 
+    def manhattanDistance(self):
+        distance = 0
+        state = np.array(self.board)
+        goal = np.array(self.goalState)
+        for num in range(1, 9):
+            statePos = np.argwhere(state == num)
+            goalPos = np.argwhere(goal == num)
+            distance += np.abs(statePos[0][0] - goalPos[0][0]) + np.abs(statePos[0][1] - goalPos[0][1])
+        return int(distance)
+
     def AStarSearch(self):
-        # Implement A* search algorithm
-        possibleMoves = self.getPossibleMoves()
-        for move in possibleMoves:
-            if move.isGoalState():
-                print("Found goal state:")
-                move.printBoard()
+        openList = []
+        closedList = []
+        heapq.heappush(openList, (self.fVal, self))
+
+        while openList:
+            _, current = heapq.heappop(openList)
+            if current.isGoalState():
+                path = []
+                while current:
+                    path.append(current)
+                    current = current.parent
+                path.reverse()
+                print("Solution found!")
+                counter = 0
+                for step in path:
+                    if counter == 0:
+                        print(f"Initial State:")
+                    else:
+                        print(f"Step {counter}:")
+                    step.printBoard()
+                    counter += 1
+                print(f"Total moves: {len(path) - 1}")
+                return
+
+            closedList.append(tuple(map(tuple, current.board)))
+
+            possibleMoves = current.getPossibleMoves()
+            for move in possibleMoves:
+                heapq.heappush(openList, (move.fVal, move))
+
+        print("No solution found.")
+        return
 
 
-TilePuzzle = EightPuzzle(initialState)
-TilePuzzle.printBoard()
+
+
+TilePuzzle = EightPuzzle(initialState21)
 TilePuzzle.AStarSearch()
